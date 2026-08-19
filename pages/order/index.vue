5e3ceecb3dc6d5f37d34d367588093c1bfcff02b
Azurem <3281186790@qq.com>
2026-08-19 15:58:04 +0800

    feat：新增路由page + 菜单 tabbar导航栏 1 新建目录+页面 2 新增tabbar：配置图标

---
+++ pages/order/index.vue
@@ -0,0 +1,13 @@
+<template>
+	<view class="">
+		<text>订单</text>
+	</view>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/user/index.vue
@@ -0,0 +1,13 @@
+<template>
+	<view class="">
+		<text>用户</text>
+	</view>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
