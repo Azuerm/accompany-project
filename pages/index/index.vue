04f4e7d8afe5366d9ab85761bd33fee0c51ad60c
Azurem <3281186790@qq.com>
2026-08-19 15:13:11 +0800

    feat：uniapp+vue3语法熟悉

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -4,23 +4,49 @@
 		<view class="text-area">
 			<text class="title">{{title}}</text>
 		</view>
+    <button @click="changeNumber">添加</button>
+    <view>水果总数：{{ totalNumber }}</view>
+    <view v-for="item in list" :key="item.name">
+      <text>{{ item.name }}</text>
+      <text>{{ item.number }}</text>
+    </view>
 	</view>
 </template>
 
-<script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
+<script setup>
+	// export default {
+	// 	data() {
+	// 		return {
+	// 			title: 'Hello'
+	// 		}
+	// 	},
+	// 	onLoad() {
 
-		},
-		methods: {
+	// 	},
+	// 	methods: {
 
-		}
-	}
+	// 	}
+	// }
+  import { ref, reactive, computed } from 'vue'
+  import { onLoad} from '@dcloudio/uni-app'
+  const title = ref('Hello')
+  const list = reactive([
+    { name: '苹果', number: 1},
+    { name: '香蕉', number: 2},
+    { name: '橙子', number: 3},
+    { name: '葡萄', number: 4},
+  ])
+  const changeNumber = () => {
+    list.forEach(item => {
+      item.number++;
+    })
+  }
+  const totalNumber = computed(() => {
+    return list.reduce((total, cur) => total + cur.number, 0)
+  })
+  onLoad(() => {
+    console.log('页面级生命周期onLoad，title.value', title.value);
+  })
 </script>
 
 <style>
