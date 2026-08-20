e87230947409a97e5623328013d2694383ecde6a
Azurem <3281186790@qq.com>
2026-08-20 14:33:00 +0800

    feat：navbar界面新增首页显示内容 1.isHome为true显示地图+搜索框 2.获取胶囊按钮的信息getMenuButtonBoundingClientRect 3.根据胶囊按钮的信息 设置 搜索框的位置

--- components/navbar/navbar.vue
+++ components/navbar/navbar.vue
@@ -1,12 +1,39 @@
 <template>
-	<view class="nav">
+	<view class="nav" :style="textStyle">
 		<!-- 状态栏 -->
 		<view :style="'height:' + status + 'rpx;' + backStyle"></view>
 		<!-- 标题栏 -->
-		<view class="navbar" :style="'height:' + navHeight + 'rpx;' + backStyle ">
+    <view v-if="isHome" class="headernav" :style="'height:' + navHeight + 'rpx;line-height:' + navHeight + 'rpx;padding-left: 20rpx;' + backStyle ">
+      <view class="city">
+        <image
+          src="/static/resource/navbar_map.png"
+        />
+        中部地区
+        <image
+          src="/static/resource/navbar_left.png"
+          class="right-icon"
+        />
+      </view>
+      <view style="flex: 1">
+        <!-- 跳转搜索页面，跟胶囊按钮的位置对齐 -->
+         <!-- margin-top: 胶囊按钮的top值 * 2 - 状态栏高度；因为胶囊按钮的top值包括状态栏的高度，所以要减去状态栏的高度 -->
+        <navigator
+          url="/pages/search/index"
+          :style="'height:'+ (menu.height*2) +'rpx;line-height:'+ (menu.height*2) +'rpx;margin-top:'+(menu.top*2 - status) + 'rpx; margin-right:' + (menu.width*2 + 24) + 'rpx;background: #f4f4f4;border-radius:200rpx; text-align:center'"
+        >
+          <view class="search-text">
+            <image
+              src="/static/resource/navbar_search.png"
+            />
+            搜索
+          </view>
+        </navigator>
+      </view>
+    </view>
+		<view v-else class="navbar" :style="'height:' + navHeight + 'rpx;' + backStyle ">
 			<view class="back-icon" @click="backOrHome">
 				<!-- 根据页面栈的数量getCurrentPages().length 判断是显示返回图标 还是 返回首页图标 -->
-				<image v-if="pages > 1" src="/static/resource/back.png" alt=""/>
+				<image v-if="pages > 1" src="/static/resource/back.png" alt="" class="back-left"/>
 				<image v-else src="/static/resource/backhome.png" alt="" />
 			</view>
       <view class="nav-title" v-if="titleText">
@@ -19,6 +46,7 @@
 <script setup>
 	import {
 		ref,
+    reactive,
 		onBeforeMount,
 		defineProps
 	} from 'vue'
@@ -46,6 +74,10 @@
     titleText: {
       type: String,
       default: ''
+    },
+    isHome: {
+      type: Boolean,
+      default: false
     }
 	})
 	// 在组件挂载前计算好状态栏高度
@@ -59,7 +91,18 @@
 	const status = ref(0)
 	// 标题栏高度
 	const navHeight = ref(0)
-
+  // 背景颜色
+	const backStyle = ref('')
+	// 字体样式
+	const textStyle = ref('')
+	// 图标样式
+	const iconStyle = ref('')
+  // 页面栈的数量
+	const pages = ref(getCurrentPages().length)
+	console.log('页面栈长度', pages.value);
+  // 胶囊按钮的位置
+  const menu = reactive( uni.getMenuButtonBoundingClientRect( ) )
+  console.log('胶囊按钮位置', menu);
 	// 计算状态栏高度
 	const setNavSize = () => {
 		// 获取系统版本和状态栏高度
@@ -79,23 +122,13 @@
 			navHeight.value = 88
 		}
 	}
-
-	// 背景颜色
-	const backStyle = ref('')
-	// 字体样式
-	const textStyle = ref('')
-	// 图标样式
-	const iconStyle = ref('')
 	// 样式设置
 	const setStyle = () => {
 		backStyle.value = ['background:' + props.background].join(';')
 		textStyle.value = ['color:' + props.color, 'font-size:' + props.fontSize + 'rpx'].join(';')
 		iconStyle.value = ['height:' + props.iconHeight + 'rpx', 'width:' + props.iconWidth + 'rpx'].join(';')
 	}
-
-	// 页面栈的数量
-	const pages = ref(getCurrentPages().length)
-	console.log('页面栈长度', pages.value);
+  // 返回按钮点击事件
 	const backOrHome = () => {
 		if (pages.value > 1) {
 			uni.navigateBack()
@@ -129,6 +162,9 @@
 		width: 64rpx;
 		height: 64rpx;
 	}
+  .back-icon .back-left {
+    transform: scale(0.7);
+  }
   .navbar {
     position: relative;
     display: flex;
@@ -139,5 +175,31 @@
     top: 0;
     left: 50%;
     transform: translate(-50%);
+  }
+  .headernav {
+    display: flex;
+  }
+  .city {
+    display: flex;
+    align-items: center;
+
+  }
+  .city image {
+    width: 64rpx;
+		height: 64rpx;
+  }
+  .city .right-icon {
+    transform: scale(0.5);
+    margin-left: -16rpx;
+  }
+  .search-text  {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .search-text image {
+    width: 64rpx;
+    height: 64rpx;
+    transform: scale(0.6);
   }
 </style>
