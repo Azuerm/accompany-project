e2f434100080815b94f509a679d08639e8f699eb
Azurem <3281186790@qq.com>
2026-08-27 16:56:30 +0800

    feat：订单详情-在线支付完善

--- pages/order/orderPage.vue
+++ pages/order/orderPage.vue
@@ -22,17 +22,46 @@
 					<text class="od-item-status">服务完成</text>
 				</view>
 			</view>
-			<!-- 支付倒计时 -->
-			<view class="od-countdown" v-if="orderList.trade_state == '待支付' && orderList._exp_time > 0">
-				<text>支付剩余时间：{{ formatCountdown(countdownMap['detail']) }}</text>
-			</view>
 		</view>
+    <view class="order-status">
+      <view class="order-box">
+        <!-- 待支付 -->
+        <block v-if="currentProgress == 2">
+          <view class="order-status-text">
+            <text>订单待支付</text>
+          </view>
+          <view class="order-time">
+            请在 {{ formatCountdown(countdownMap['detail']) }} 内完成支付，超时订单将自动取消
+          </view>
+          <view class="order-btn">
+            <button @click="dopay">立即支付 ({{ orderList.price }}元)</button>
+          </view>
+        </block>
+      </view>
+    </view>
 
+     <!-- 二维码弹出层 -->
+    <uni-popup ref="popupQrCode" type="center" :is-mask-click="false" background-color="#fff" border-radius=" 20rpx">
+      <view class="pay-box">
+        <view class="pay-icon">
+          <image
+            :src="cancelIcon"
+            style="display: inline-block; width: 30rpx; height: 30rpx; "
+            @click="payment"
+          />
+        </view>
+        <view style="text-align: center; padding-bottom: 20rpx;">微信支付</view>
+        <canvas id="qrcode" canvas-id="qrcode" style="width: 300rpx;height: 300rpx; "></canvas>
+        <view style="text-align: center; padding: 20rpx 0;" >请用本人微信扫描以上二维码</view>
+      </view>
+    </uni-popup>
 	</view>
 </template>
 
 <script setup>
 	import bannerBg from '@/static/resource/banner-bg.jpg'
+  import cancelIcon from '@/static/resource/cancel.png'
+  import UQRCode from 'uqrcodejs'
 	import {
 		ref,
     computed
@@ -44,6 +73,8 @@
 	} from '@dcloudio/uni-app'
   import { useCountdown } from '@/hooks/useCountdown'
   const app = getApp()
+  const { windowWidth } = uni.getSystemInfoSync()
+  const rpxRatio = 750 / windowWidth
   // 倒计时逻辑抽离到hooks/useCountdown.js，供订单列表页和订单详情页共用
   const { countdownMap, startCountdown, stopCountdown, formatCountdown } = useCountdown()
   // 订单数据
@@ -59,6 +90,8 @@
 	const currentPercent = computed(() => stateMap[orderList.value.trade_state]?.percent ?? 0) // 变量 ?? 默认值
 	// 当前进度文字
 	const currentProgress = computed(() => stateMap[orderList.value.trade_state]?.progress ?? 12.8)
+  // 二维码弹窗
+  const popupQrCode = ref()
   onLoad((params) => {
     getOrderDetail(params)
   })
@@ -81,6 +114,29 @@
         }
       }
     })
+  }
+  // 点击立即支付
+  const dopay = () => {
+    // 打开二维码弹窗
+    popupQrCode.value.open()
+    var qr = new UQRCode();
+    // 设置二维码内容
+    qr.data = orderList.value.code_url;
+    // 设置二维码大小，必须与canvas设置的宽高一致
+    qr.size = Math.floor(300 / rpxRatio);
+    // 调用制作二维码方法
+    qr.make();
+    // 获取canvas上下文
+    var canvasContext = uni.createCanvasContext('qrcode'); // 如果是组件，this必须传入
+    // 设置***实例的canvas上下文
+    qr.canvasContext = canvasContext;
+    // 调用绘制方法将二维码图案绘制到canvas上
+    qr.drawCanvas();
+  }
+  // 关闭二维码弹窗
+  const payment = () => {
+    popupQrCode.value.close()
+    uni.switchTab({ url: '/pages/order/index' })
   }
   // 页面隐藏或卸载时清除定时器
   onHide(() => {
@@ -150,10 +206,54 @@
  		// font-weight: bold;
  	}
 
- 	// 支付倒计时
- 	.od-countdown {
- 		margin-top: 10rpx;
- 		font-size: 26rpx;
- 		color: #ffa200;
- 	}
+ 	
+  .order-status {
+    position: relative;
+		margin-top: -40rpx;
+    width: 100%;
+    border-radius: 20rpx 20rpx 0 0;
+    height: auto;
+    background-color: #fff;
+  }
+  .order-status .order-box {
+    padding: 40rpx;
+    display: flex;
+    flex-direction: column;
+  }
+  .order-box .order-status-text {
+    flex: 1;
+    font-weight: bold;
+    font-size: 40rpx;
+  }
+  .order-box .order-time {
+    font-size: 28rpx;
+    margin-top: 20rpx;
+  }
+  .order-box .order-btn {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 40rpx;
+  }
+  .order-btn button {
+    background-color: #53B286;
+    color: #fff;
+    font-weight: bold;
+    font-size: 32rpx;
+    width: 80%;
+  }
+  // 二维码
+  .pay-box {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 20rpx;
+  }
+  .pay-box .pay-icon {
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+  }
 </style>
